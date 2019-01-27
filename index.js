78fb145ea6b8714db9ab938255e1e9ad2225061d
Sean Tayler <38925176+seantayler@users.noreply.github.com>
2019-01-27 08:59:39 -0700

    added fibonacci (recursive)

--- index.js
+++ index.js
@@ -530,7 +530,12 @@ function fib(n){
 
 Steps (solution #2) - recursive
 
-
+function fib(n){
+  if (n < 2){
+    return n;
+  }
+  return fib(n-1) + fib(n-2)
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
