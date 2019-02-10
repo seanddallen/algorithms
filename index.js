4460eb06f0e2dcdc0d368f24195ab29177d99bb0
Sean Tayler <38925176+seantayler@users.noreply.github.com>
2019-02-10 14:56:46 -0700

    added Absolute Value Sum Minimization

--- index.js
+++ index.js
@@ -728,6 +728,18 @@ function fib(n){
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//Absolute Values Sum Minimization
+
+//Instructions: Given a sorted array of integers a, find an integer x from a such that the value of    
+	//abs(a[0] – x) + abs(a[1] – x) + … + abs(a[a.length – 1] – x)   
+//is the smallest possible (abs = absolute value). Basically return the median value. If there are several possible answers output the smallest one. 
+
+//Steps (solution #1)
+
+function absoluteValueSumMinimization(a){
+  const isEven = a.length % 2 === 0; 
+  return isEven ? a[a.length/2 - 1] : a[Math.floor(a.length / 2)]
+}
 
 
 
