c469d37c17f76bfb510a055992766a0a27f65f51
Sean Tayler <38925176+seantayler@users.noreply.github.com>
2019-02-23 12:33:33 -0700

    updated almostIncreasingSequence

--- index.js
+++ index.js
@@ -865,6 +865,9 @@ function almostIncreasingSequence(arr){
   let count = 0;
   
   for(let i = 0; i < arr.length; i++){
+    if (arr[i] === arr[i+2]) {
+      return false
+    }
     if (arr[i] < arr[i+1]){
       console.log(arr[i])
     } else if (arr[i] >= arr[i+1]){
