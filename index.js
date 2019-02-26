0e26ed90c8c720974e0dff41839795e2805b983d
Sean Tayler <38925176+seantayler@users.noreply.github.com>
2019-02-26 09:32:00 -0700

    added Alternating Sums

--- index.js
+++ index.js
@@ -929,7 +929,25 @@ function alphabetSubsequence(str){
 ////////////////////////////////////////////////////////////////////////////////
 
 
+//Alternating Sums
 
+//Instructions: Several people are standing in a row and need to be divided into two teams. The first person goes into team 1, the second goes into team 2, the third to team one, etc. You are given an array of positive integers – the weights of people. Return an array of two integers, where the first element is the total weight of team 1, and the second element is the total weight of team 2 after the division is complete. 
+
+//Steps (solution #1)
+
+function alternatingSums(arr){
+  let group1 = 0;
+  let group2 = 0;
+
+  for (let i = 0; i < arr.length; i++){
+    if (i % 2 === 0){
+      group1 += arr[i]
+    } else {
+      group2 += arr[i]
+    }
+  }
+  return [group1, group2]
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
