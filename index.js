b013b398e3538260bf6dd5ad2a77082b0b63e8f5
Sean Tayler <seanjtayler@gmail.com>
2019-03-02 14:58:05 -0700

    added Are Similar

--- index.js
+++ index.js
@@ -985,7 +985,35 @@ function areEquallyStrong(myLeft, myRight, friendLeft, friendRight){
 ////////////////////////////////////////////////////////////////////////////////
 
 
+//Are Similar
 
+//Instructions: Given two arrays check if they are similar. Two arrays are called similar if one can be obtained from another by swapping at most one pair of elements in one of the arrays. 
+
+//Steps (solution #1)
+
+function areSimilar(a, b){
+  let c = []
+  let d = []
+
+  if(a.toString() === b.toString()){
+    return true
+  }
+
+  for (let i = 0; i < a.length; i++){
+    if (a[i] !== b[i]){
+      c.push(a[i])
+      d.push(b[i])
+    }
+  }
+
+  d = d.reverse()
+
+  if(c.length === 2 && (c.toString() === d.toString())){
+    return true
+  }
+
+  return false
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
