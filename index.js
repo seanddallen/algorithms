c962361499baae6dad41c7ef5b082d7a74a1945b
Sean Tayler <seanjtayler@gmail.com>
2019-04-05 21:32:36 -0700

    added Bishop and Pawn

--- index.js
+++ index.js
@@ -1224,7 +1224,35 @@ function avoidObstacles(arr){
 ////////////////////////////////////////////////////////////////////////////////
 
 
+//Bishop and Pawn
 
+//Instructions: given the positions of a white bishop and a black pawn on the standard chess board, determine whether the bishop	can capture the pawn in one move. The bishop has no restrictions in distance for each move, but it is limited to diagonal movement. 
+
+//Steps (solution #1)
+
+function bishopAndPawn(bishop, pawn){
+  const board = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8
+  };
+
+  const bishopX = board[bishop[0]]
+  const bishopY = parseInt(bishop[1])
+  const pawnX = board[pawn[0]]
+  const pawnY = parseInt(pawn[1])
+
+  if(bishopX + bishopY === pawnX + pawnY || bishopX + pawnY === pawnX + bishopY){
+    return true;
+  }
+
+  return false; 
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
