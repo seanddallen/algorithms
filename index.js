8c74c9a9f89e4237bf49c408fd1a36a4bf5fbd38
Sean Tayler <seanjtayler@gmail.com>
2019-04-15 18:09:51 -0700

    added Box Blur

--- index.js
+++ index.js
@@ -1355,7 +1355,40 @@ let mode = function(arr){
 ////////////////////////////////////////////////////////////////////////////////
 	
 	
-	
+//Box Blur
+
+//Instructions: Last night you had to study, but decided to party instead. Now there is a black and white photo of you that is about to go viral. You cannot let this ruin your reputation, so you want to apply box blur algorithm to the photo to hide its content. The algorithm works as follows: each pixel x in the resulting image has a value equal to the average value of the input image pixels’ values from the 3x3 square with the center at x. All pixels at the edges are cropped. As pixel’s value is an integer, all fractions should be rounded down. 
+
+/* EX: [[1,1,1],
+     [1,7,1],
+     [1,1,1]]
+Output: [[1]] (15/9 rounded down) */
+
+//Steps (solution #1)
+
+function boxBlur(image){
+  const res = []
+
+  for(let y = 0; y < image.length - 2; y++){
+    const line = []
+    
+    for(let x = 0; x < image[y].length - 2; x++){
+      let sum = 0
+      let count = 0
+
+      for(let a = y; a < y + 3; a++){
+        for(let b = x; b < x + 3; b++){
+          sum += image[a][b]
+          count++
+        }
+      }
+
+      line.push(Math.floor(sum/count))
+    }
+    res.push(line)
+  }
+  return res
+}	
 	
 	
 ////////////////////////////////////////////////////////////////////////////////
