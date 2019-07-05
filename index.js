f8ffa2a437b01df3b5b1558075f5b8f60108e3f4
Sean Tayler <seanjtayler@gmail.com>
2019-07-04 19:12:34 -0700

    added Needle in Haystack

--- index.js
+++ index.js
@@ -1710,13 +1710,18 @@ function greet(name){
 ////////////////////////////////////////////////////////////////////////////////
 
 
-//Sum of Positive (Kata - 8)
+//Needle in Haystack (Kata - 8)
 	
-//Instructions: You get an array of numbers, return the sum of all of the positives ones.
+//Instructions: Can you find the needle in the haystack?
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//After your function finds the needle it should return a message (as a string) that says:
+//"found the needle at position " plus the index it found 
 
 //Solution #1: 
 	
-
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 	
