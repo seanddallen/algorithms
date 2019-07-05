ae1c5e746d3b4af569d2401b2c050494e7d7154e
Sean Tayler <seanjtayler@gmail.com>
2019-07-04 19:14:45 -0700

    added Keep Hydrated

--- index.js
+++ index.js
@@ -1726,12 +1726,22 @@ function findNeedle(haystack) {
 ////////////////////////////////////////////////////////////////////////////////
 	
 	
-//Sum of Positive (Kata - 8)
+//Keep Hydrated (Kata - 8)
 	
-//Instructions: You get an array of numbers, return the sum of all of the positives ones.
+//Instructions: Nathan loves cycling.
+//Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+//You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+//EX:
+//time = 3 ----> litres = 1
+//time = 6.7---> litres = 3
+//time = 11.8--> litres = 5
 
 //Solution #1: 
-		
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
 	
 	
 ////////////////////////////////////////////////////////////////////////////////
