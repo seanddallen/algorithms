388bbd5ed6e2d7b480bb69aae93b384c074105fb
Sean Tayler <seanjtayler@gmail.com>
2019-07-07 09:28:32 -0700

    added Highest and Lowest Numbers

--- index.js
+++ index.js
@@ -2047,13 +2047,26 @@ function getMiddle(s){
 ////////////////////////////////////////////////////////////////////////////////
 	
 	
-//Sum of Positive (Kata - 7)
+//Highest and Lowest Numbers (Kata - 7)
 	
-//Instructions: You get an array of numbers, return the sum of all of the positives ones.
+//Instructions: You are given a string of space separated numbers, and have to return the highest and lowest number.
+//All numbers are valid Int32, no need to validate them.
+//There will always be at least one number in the input string.
+//Output string must be two numbers separated by a single space, and highest number is first.
+//EX: highAndLow("1 2 3 4 5");  // return "5 1"
 
 //Solution #1: 
-		
 	
+function highAndLow(numbers){
+  return [Math.max(...numbers.split(' ')), ' ', Math.min(...numbers.split(' '))].join('')
+}
+
+//Solution #1: 
+
+function highAndLow(numbers){
+  var arr = numbers.split(' ').sort((a, b) => a - b);
+  return arr[arr.length -1] + ' ' + arr[0];
+}
 	
 ////////////////////////////////////////////////////////////////////////////////
 
