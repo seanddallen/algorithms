dd691ce2eab1a5f2ff00651a84fbe5649081e6a8
Sean Tayler <seanjtayler@gmail.com>
2019-07-06 22:52:58 -0700

    added Get the Middle

--- index.js
+++ index.js
@@ -2032,13 +2032,17 @@ function accum(s) {
 ////////////////////////////////////////////////////////////////////////////////
 
 
-//Sum of Positive (Kata - 7)
+//Get the Middle (Kata - 7)
 	
-//Instructions: You get an array of numbers, return the sum of all of the positives ones.
+//Instructions: You are going to be given a word. Your job is to return the middle character of the word. 
+//If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
 
 //Solution #1: 
 	
-
+function getMiddle(s){
+ return s.length % 2 === 0 ? s[s.length/2 - 1] + s[s.length/2] : s[Math.ceil(s.length/2)]
+}
+	
 
 ////////////////////////////////////////////////////////////////////////////////
 	
