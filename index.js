cccd4ac63aa87a268e6eae48a9b264fd28420520
Sean Tayler <seanjtayler@gmail.com>
2019-07-18 23:31:53 -0700

    added Multiples of 3 or 5

--- index.js
+++ index.js
@@ -2291,7 +2291,7 @@ function highestScoringWord(str){
 
 ////////////////////////////////////////////////////////////////////////////////
 	
-//Find Odd Int (Kata - 7)
+//Find Odd Int (Kata - 6)
 	
 //Instructions: Given an array, find the int that appears an odd number of times.
 //There will always be only one integer that appears an odd number of times.
@@ -2316,14 +2316,41 @@ function findOdd(A) {
 ////////////////////////////////////////////////////////////////////////////////
 
 
-//Sum of Positive (Kata - 6)
+//Multiples of 3 or 5 (Kata - 6)
 	
-//Instructions: You get an array of numbers, return the sum of all of the positives ones.
+//Instructions: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//Note: If the number is a multiple of both 3 and 5, only count it once.
 
 //Solution #1: 
 	
+function solution(number){
+  let sum = 0;
+  for(let i = 0; i < number; i++){
+    if(i % 3 === 0 && i % 5 === 0 ){
+      sum += i
+    } else if(i % 3 === 0){
+      sum += i
+    } else if(i % 5 === 0){
+      sum += i
+    }
+  }
+  return sum
+}
 
+//Solution #2: 
 
+function solution(number){
+  var sum = 0;
+  for(var i = 1;i< number; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+      sum += i
+    }
+  }
+  return sum;
+}
+
+	
 ////////////////////////////////////////////////////////////////////////////////
 
 
